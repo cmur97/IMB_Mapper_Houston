a5b3c08157a8ad3028d12b12e667393308cb1468
cmur97 <63924852+cmur97@users.noreply.github.com>
2021-06-19 01:36:04 +0900

    Add files via upload

---
+++ HoustonWebScraper.py
@@ -0,0 +1,47 @@
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+
+# Scraped data will be added to these dictionaries
+names = []
+phone_numbers = []
+addresses = []
+
+# Page range
+pages = range(1, 51)
+
+# Loop through each page
+for num in pages:
+
+    page = requests.get("https://www.rubmaps.ch/houston-massage-parlors-tx-" + str(num) + "#rubmaps")
+    soup = BeautifulSoup(page.text, 'html.parser')
+    IMB_address_list = soup.find_all('div', class_='rows')
+
+    for container in IMB_address_list:
+        name = container.find('a', class_='th-a').text
+        names.append(name)
+
+        phone_num = container.find('i', class_='phonenumberrow').text
+        phone_numbers.append(phone_num)
+
+        address = container.br.next_element.strip()
+        full_address = address + " Houston, TX"
+        addresses.append(full_address)
+
+# Create a Pandas DataFrame populated with pertinent info
+IMB_List = pd.DataFrame({
+    'Name': names,
+    'Phone Numbers': phone_numbers,
+    'Address': addresses
+})
+
+
+
+
+
+
+
+
+
