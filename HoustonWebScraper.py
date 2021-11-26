eaeee19002e89b5378e3352d6016d4695a705b37
cmur97 <63924852+cmur97@users.noreply.github.com>
2021-11-27 08:19:00 +0900

    Update HoustonWebScraper.py

--- HoustonWebScraper.py
+++ HoustonWebScraper.py
@@ -29,7 +29,7 @@ for num in pages:
         full_address = address + " Houston, TX"
         addresses.append(full_address)
 
-# Create a Pandas DataFrame populated with pertinent info
+# Create a Pandas DataFrame populated with relevant info
 IMB_List = pd.DataFrame({
     'Name': names,
     'Phone Numbers': phone_numbers,
@@ -42,6 +42,43 @@ IMB_List.to_csv('IMB_list_Houston.csv', index=False)
 # Only addresses for map
 only_addresses = IMB_List['Address']
 only_addresses.to_csv('IMB_Addresses.csv', index=False)
+
+# Find duplicates of phone numbers and addresses 
+
+# Phone Numbers
+def getDuplicatesWithCount(phone_numbers):
+    dictofElems = dict()
+    for elem in phone_numbers:
+        if elem in dictofElems:
+            dictofElems[elem] += 1
+        else:
+            dictofElems[elem] = 1
+
+    dictofElems = {key: value for key, value in dictofElems.items() if value > 1}
+    return dictofElems
+
+
+dictofElems = getDuplicatesWithCount(phone_numbers)
+for key, value in dictofElems.items():
+    print(key, ' :: ', value)
+
+# Addresses
+def getDuplicatesWithCount(addresses):
+    dictofElems = dict()
+    for elem in addresses:
+        if elem in dictofElems:
+            dictofElems[elem] += 1
+        else:
+            dictofElems[elem] = 1
+
+    dictofElems = {key: value for key, value in dictofElems.items() if value > 1}
+    return dictofElems
+
+
+dictofElems = getDuplicatesWithCount(addresses)
+for key, value in dictofElems.items():
+    print(key, ' :: ', value)
+
 
 
 
